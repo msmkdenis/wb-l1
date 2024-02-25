6c880f423208947b38fc3d02b79cdae5115250bb
msmkdenis <msmkdenis@gmail.com>
2024-02-25 19:49:00 +0300

    1-3

--- l1-3.go
+++ l1-3.go
@@ -11,25 +11,30 @@ import (
 
 func main() {
 	a := []int{2, 4, 6, 8, 10}
-	wg := sync.WaitGroup{}
-	fmt.Println("---Второй способ - отправляем результат в канал и читаем из него в другой горутине---")
 
 	res := make(chan int)
-	go func() { // в отдельной горутине запустим чтение из канала и выведем результат
-		var sum int
-		for r := range res {
-			sum += r
+	go func() {
+		wg := &sync.WaitGroup{}
+		wg.Add(len(a))
+		for i := 0; i < len(a); i++ {
+			go func(i int) {
+				defer wg.Done()
+				res <- a[i] * a[i]
+			}(i)
 		}
-		println(sum)
+		wg.Wait()
+		close(res) // закрываем канал т.к. мы уверены, что больше писать в него не будем
 	}()
 
-	wg.Add(len(a))
-	for i := 0; i < len(a); i++ {
-		go func(i int) {
-			defer wg.Done()
-			res <- a[i] * a[i]
-		}(i)
-	}
-	wg.Wait()  // ждем окончания работы цикла
-	close(res) // закрываем канал т.к. мы уверены, что больше писать в него не будем
+	wg := &sync.WaitGroup{}
+	wg.Add(1)
+	go func() { // в отдельной горутине запустим чтение из канала и выведем результат
+		var sum int
+		defer wg.Done()
+		for r := range res { // пока не будет закрыт канал - чтение будет продолжаться
+			sum += r
+		}
+		fmt.Println(sum)
+	}()
+	wg.Wait() // дождем вывода результата
 }
